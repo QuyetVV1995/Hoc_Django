67ba96de9c0b60d1baf99995107b30212dd9f714
QuyetVV95 <quyeta2ubqn@gmail.com>
2019-12-11 22:41:14 +0700

    Namespace va static

--- myproject/mysite/polls/urls.py
+++ myproject/mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.views.generic import TemplateView
 
+app_name = 'polls'
 urlpatterns = [
     path('detail/<int:question_id>', views.detailView, name='detail_view'),
     path('list/', views.viewList, name='view_list'),
