fd6a42a28a5ca761988fcda14ff5e3a2f2c40c7f
QuyetVV95 <quyeta2ubqn@gmail.com>
2019-12-11 15:11:51 +0700

    Models in Django: create database

--- myproject/mysite/polls/models.py
+++ myproject/mysite/polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+# models là phần ánh xạ cơ sở dữ liệu tu python
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    time_pub = models.DateTimeField()
+    # Khi luu tru trong database se tu dong co them 1 truong ID
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)    # Cascade: Khi Question xoa, thi Choice cung bi xoa theo
+    choice_text = models.CharField(max_length=100)
+    vote = models.IntegerField(default=0)
+
+# tao migrations bang command: python manage.py makemigrations
+# tao database: python manage.py migrate
