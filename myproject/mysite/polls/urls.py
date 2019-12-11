b270f9930eff1cac4c28b1e47517960abb439b3e
QuyetVV95 <quyeta2ubqn@gmail.com>
2019-12-11 16:35:30 +0700

    Templates part3: add question_list.html

--- myproject/mysite/polls/urls.py
+++ myproject/mysite/polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('list/', views.viewList, name='view_list'),
     path('', views.index, name='index'),    # dky duong dan
 ]
--- myproject/mysite/polls/views.py
+++ myproject/mysite/polls/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from  django.http import HttpResponse
+from django.http import HttpResponse
+from .models import Question
+
+
 # Create your views here.
 
-def index(request):         # dinh nghia ham
+def index(request):  # dinh nghia ham
     myname = "Vu Van Quyet";
     list_item = ['Xe may', 'Dien thoai', 'laptop']
-    return render(request,"polls/index.html",{"v_name": myname, "v_item":list_item});
+    return render(request, "polls/index.html", {"v_name": myname, "v_item": list_item});
+
+
+def viewList(request):
+    v_listQuestion = Question.objects.all()
+    context = {"listques": v_listQuestion}
+    # return render(request, "polls/question_list","listques":v_listQuestion)
+    return render(request, "polls/question_list.html", context)
