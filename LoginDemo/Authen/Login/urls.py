e78999cce79f214213d2a418474a6109c2c7c560
QuyetVV95 <quyeta2ubqn@gmail.com>
2019-12-15 22:20:25 +0700

    Check User permissions

--- LoginDemo/Authen/Login/urls.py
+++ LoginDemo/Authen/Login/urls.py
@@ -1,12 +1,13 @@
-from .views import IndexClass, LoginClass, ViewUser, view_product
+from .views import IndexClass, LoginClass, ViewUser, view_product, addPost
 from django.urls import path
 
-
+app_name ='Login'
 urlpatterns = [
     path('', IndexClass.as_view(), name='index'),
     path('login/', LoginClass.as_view(), name='login'),
-    path('viewuser', ViewUser.as_view(), name='viewuser'),
-    path('viewproduct',view_product, name='viewproduct'),
+    path('viewuser/', ViewUser.as_view(), name='viewuser'),
+    path('viewproduct/',view_product, name='viewproduct'),
+    path('addpost/', addPost.as_view(), name='addpost'),
 
 
 ]
--- LoginDemo/Authen/Login/views.py
+++ LoginDemo/Authen/Login/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 from django.views import View
 from django.contrib.auth import authenticate, login, decorators
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import PostForm
+
 # Create your views here.
 
 
@@ -33,3 +35,22 @@ class ViewUser(LoginRequiredMixin,View):    # Luu y nen de LoginRequiredMixin tr
 @decorators.login_required(login_url='/login/')
 def view_product(request):
     return HttpResponse('View Product')
+
+
+class addPost(LoginRequiredMixin, View):
+    login_url = '/login/'           # Yeu cau login
+
+    def get(self, request):
+        f = PostForm()
+        context = {'fm': f}
+        return render(request,'Login/addpost.html', context)
+    def post(self, request):
+        f = PostForm(request.POST)      # Bat du lieu nhap vao Postform
+        if not f.is_valid():
+            return HttpResponse('Nhap sai du lieu')
+        print(request.user.get_all_permissions())        # In ra ca quyen cua User
+        if request.user.has_perm('Login.add_post'):     # cu phap: Login.add_post = name_app.permission_name_models
+            f.save()        # Luu du lieu vao database
+        else:
+            return HttpResponse('No permisson add post')
+        return HttpResponse('Luu du lieu thanh cong')
